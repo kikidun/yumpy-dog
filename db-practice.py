3441e5a247a827b64e1717298790ddb92249f4e5
Kieran Dunbar <kierandunbar@gmail.com>
2025-11-05 20:38:30 -0600

    query fix with conn.commit()

--- db-practice.py
+++ db-practice.py
@@ -11,13 +11,15 @@ conn = psycopg2.connect(
     password="pwtest"
 )
 
-cur = conn.cursor()
+
 print("connected to DB")
 
 def getConfig():
+    cur = conn.cursor()
     query = "SELECT * FROM config"
     cur.execute(query)
     config = cur.fetchall()
+    cur.close()
     return config
 
 config = getConfig()
@@ -27,30 +29,28 @@ print("printing configuration:")
 for key in config:
     print(key[0] + ": " + key[1])
 
-
-test = getConfig()[0][1]
-print("test")
-print(test)
-
 #Retrieves the entire monitor table to get urls from. 
-#todo: look at refining
 def getMonitors():
+    cur = conn.cursor()
     query = "SELECT * FROM monitors"
     cur.execute(query)
     monitors = cur.fetchall()
-
+    cur.close()
     return monitors
 
 #query the monitor table for entries where last checked + interval is less than now
 def getMonitorsForChecking():
-    query = "SELECT * FROM monitors WHERE last_checked + (interval * INTERVAL '1 second') < CURRENT_TIMESTAMP;"
-    cur.execute(query)
-    monitors = cur.fetchall()
+    conn.commit()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM monitors m WHERE m.last_checked + (m.interval * INTERVAL '1 second') < CURRENT_TIMESTAMP")
+    monitorsForChecking = cur.fetchall()
+    print("monitors for checking: "+ str(monitorsForChecking))
+    cur.close()
+    return monitorsForChecking
 
-    return monitors
 #HTTP get of a given URL, return response and timestamp
 def checkURL(URL):
-    print("Sending request to "+URL)
+    #print("Sending request to "+URL)
     send_timestamp = datetime.now()
     resp = requests.get(URL, verify=False)
 
@@ -58,73 +58,44 @@ def checkURL(URL):
 
 #put the result of the health check in the db, update last checked
 def update_DB(monitor, health_check_result, send_time):
+    cur = conn.cursor()
     print("Loading the database with a "+ str(health_check_result) +" for "+ monitor[1])
     try:
         cur.execute("INSERT INTO healthcheck_data ( healthcheck_timestamp, monitor_id, response, response_time) VALUES (%s, %s, %s, %s)",
         (send_time, monitor[0], health_check_result.status_code, health_check_result.elapsed))
         cur.execute("UPDATE monitors SET last_checked =%s WHERE monitor_id = %s",(send_time, monitor[0]))
         conn.commit()
-        print("Commit successful!")
+        #print("Commit successful!")
     except Exception as e:
         conn.rollback()  # Undo all changes in this transaction
         print("Commit failed!")
         print(f"Error: {e}")
+    cur.close()
     return True
 
 
-
-
-
-#every X number of seconds, query the monitor table for entries where last checked + interval is less than now
-#check those monitors
-
-#for every entry in monitors, check the url, and put the result in the db
-#todo: the loop isn't quite working, its going too fast
-
-def working_logic(monitors):
-    #if worker_enabled true
-    enabled = config[0][1] 
+def working_logic():
     while True:
+        conn.commit()
         enabled = getConfig()[0][1]
-        print(str(datetime.now())+" LOG: enabled is set to " + enabled)
+        #print(str(datetime.now())+" LOG: enabled is set to " + enabled)
         if enabled.strip() == "True":
-            #print(str(datetime.now())+" printing monitors")
-           # for row in monitors:
-                #print(row[1])
-            #print("")
-            for monitor in monitors:
-                health_check_result, send_time = checkURL(monitor[2])
-                print(health_check_result.status_code)
-                db_resp = update_DB(monitor, health_check_result, send_time)
-            print(str(datetime.now())+" LOG: Setting 'enabled' to : "+ str(getConfig()[0][1]))    
-            time.sleep(10)
+            monitors = getMonitorsForChecking()
+            #print(monitors)
+            if len(monitors) == 0:
+                time.sleep(10)
+            else:
+                for monitor in monitors:
+                    health_check_result, send_time = checkURL(monitor[2])
+                    #print(health_check_result.status_code)
+                    update_DB(monitor, health_check_result, send_time)
+            #print(str(datetime.now())+" LOG: Setting 'enabled' to : "+ str(getConfig()[0][1]))    
+                time.sleep(10)
         else:
-            print(str(datetime.now())+" LOG: because enabled is set to " + enabled + ", Sleep for 10s")
+            #print(str(datetime.now())+" LOG: because enabled is set to " + enabled + ", Sleep for 10s")
             time.sleep(10)
 
 
+working_logic()
 
-
-
-
-monitors = getMonitors()
-working_logic(monitors)
-
-cur.close()
 conn.close()
-
-# Insert data
-#cur.execute(
-#    "INSERT INTO users (name, email) VALUES (%s, %s)",
-#    ("Alice", "alice@example.com")
-#)
-
-#print("Sending to DB")
-#print( send_timestamp, resp.status_code, resp.elapsed)
-#cur.execute(
-#    "INSERT INTO healthcheck_data ( healthcheck_timestamp, monitor_id, response, response_time) VALUES (%s, %s, %s, %s)",
-#    (send_timestamp, 1, resp.status_code, resp.elapsed)
-#)
-
-# Commit changes
-#conn.commit()
